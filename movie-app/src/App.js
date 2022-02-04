803b8f3920886ad6930802dadcf8afedf03a5780
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-02-04 12:56:09 -0300

    starting to fetch for info

---
+++ movie-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Movie from './components/Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+function App() {
+  const[ movies, setMovies ] = useState([]);
+  useEffect(() => {
+    fetch(FEATURED_API)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setMovies(data);
+    });
+
+  }, [])
+
+  return (
+    <div className="App">
+      {movies.length > 0 && movies.map((movie) => <Movie />)}
+    </div>
+  );
+}
+
+export default App;
---
+++ movie-app/src/components/Movie.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Movie = () => (
+    <div className="movie">
+        Thsi is a movie component
+    </div>
+);
+
+export default Movie;
