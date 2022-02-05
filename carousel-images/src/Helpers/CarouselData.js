5a64fc4a0284fe9c31b1216439e0fb4ad164264f
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-02-04 21:48:50 -0300

    carousel image

---
+++ carousel-images/src/Helpers/CarouselData.js
@@ -0,0 +1,21 @@
+import City from '../Images/city.jpg';
+import Salvador from '../Images/salvador.jpg';
+import Ubc from '../Images/ubc.jpg';
+
+export const images = [
+    {
+        title: "San Diego", 
+        subtitle: "This is San Diego",
+        img: City,
+    },
+    {
+        title: "Salvador, Brazil",
+        subtitle: "The best city",
+        img: Salvador,
+    },
+    {
+        title: "UBC (Vancouver)",
+        subtitle: "The University of British Columbia",
+        img: Ubc,
+    },
+];
